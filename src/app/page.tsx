416dab6ac6af37b8a6c26076292d8f572425c65e
Pierre Cattin <cattin.pierre@gmail.com>
2024-02-11 19:46:32 +0100

    Login and Signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -281,13 +281,128 @@ function SideNavButton({toggleSideNav}: {toggleSideNav: any}){
   )
 }
 
-function LoginPage({ onLogin, setIsAuthenticated }: 
-  { onLogin: (username: string, password: string) => void , setIsAuthenticated: any }) {
+
+function LoginPage({
+  onLogin, onSignUpClick}: {onLogin: (username: string, password: string) => void, onSignUpClick: any }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (event: React.FormEvent) => {
+      event.preventDefault();
+      onLogin(email, password);
+    };
+    return(
+      <>
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+      <div className="w-full max-w-xs p-9 space-y-4 rounded-lg bg-gray-800 shadow-black">
+      <form onSubmit={handleSubmit} className="space-y-4 mb-10">
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-300">Email</label>
+          <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} 
+                className="mt-1 p-1 block w-full rounded-md bg-gray-700 border-gray-600 shadow-sm focus:border-purple-500 focus:ring-purple-500 text-white" 
+                placeholder="Email" required />
+        </div>
+        <div>
+          <label htmlFor="password" className="block text-sm font-medium text-gray-300">Password</label>
+          <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}
+                className="mt-1 p-1 block w-full rounded-md bg-gray-700 border-gray-600 shadow-sm focus:border-purple-500 focus:ring-purple-500 text-white" 
+                placeholder="Password" required />
+        </div>
+        <button type="submit" 
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+          Login
+        </button>
+      </form>
+      <button 
+        onClick={onSignUpClick}
+        className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
+          No account yet? Sign up!
+      </button>
+    </div>
+  </div>
+  </>
+  )
+}
+
+
+function SignUpPage({showLogin}:{showLogin: any}) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const createUser = (event: React.FormEvent) => {
+    event.preventDefault();
+    const signup = async () => {
+      const response = await fetch(`${BACKEND_URL}createuser`, {
+        method: 'POST',
+        body: String(JSON.stringify({ username, email, password })),
+        headers: {
+          'Content-Type': 'application/json', 
+        }
+      });
+    
+        if (response.ok) {
+          alert("User profile created")
+          showLogin();
+        } else {
+          const error = await response.json().then(x=>x.error)
+          alert(error)
+        }
+    };
+    signup()
+  };
+
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+    <div className="w-full max-w-xs p-9 space-y-4 rounded-lg bg-gray-800 shadow-black">
+    <form onSubmit={createUser} className="space-y-4 mb-10">
+    <h2 className="text-xl font-semibold text-gray-300">Sign Up</h2>
+      <div>
+        <label htmlFor="username" className="block text-sm font-medium text-gray-300">Username</label>
+        <input id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}
+                 className="mt-1 p-1  block w-full rounded-md bg-gray-700 border-gray-600 shadow-sm focus:border-purple-500 focus:ring-purple-500 text-white" 
+                 placeholder="Username" required />
+      </div>
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium text-gray-300">Email</label>
+        <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}
+                 className="mt-1 p-1  block w-full rounded-md bg-gray-700 border-gray-600 shadow-sm focus:border-purple-500 focus:ring-purple-500 text-white" 
+                 placeholder="Email" required />
+      </div>
+      <div>
+        <label htmlFor="password" className="block text-sm font-medium text-gray-300">Password</label>
+        <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 p-1 block w-full rounded-md bg-gray-700 border-gray-600 shadow-sm focus:border-purple-500 focus:ring-purple-500 text-white" 
+              placeholder="Password" required />
+      </div>
+      <button type="submit" 
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        Sign Up
+      </button>
+    </form>
+    <button 
+      onClick={showLogin}
+      className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
+        Return to login
+    </button>
+  </div>
+</div>
+</>
+
+  );
+}
+
+function LoginOrSignupPage({
+  onLogin,
+  setIsAuthenticated
+}: {
+  onLogin: (username: string, password: string) => void,
+  setIsAuthenticated: any
+}) {
+  const [signUpIsShown, setSignUpIsShown] = useState(false);
 
   // ByPass login if user is already authenticated (via cookie authToken)
-  const userIsAuthenticated = async () =>{
+  const userIsAuthenticated = async () => {
     fetch(`${BACKEND_URL}userisauthenticated`, {
       method: 'GET',
       credentials: 'include',
@@ -298,40 +413,44 @@ function LoginPage({ onLogin, setIsAuthenticated }:
   }
   userIsAuthenticated();
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    onLogin(username, password);
-  };
+  function showSignup(){
+    setSignUpIsShown(true)
+  }
+
+  function showLogin(){
+    setSignUpIsShown(false)
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-      <button type="submit">Login</button>
-    </form>
+    <>
+    {!signUpIsShown && <LoginPage onLogin={onLogin} onSignUpClick={showSignup}/> } 
+    {signUpIsShown && <SignUpPage showLogin={showLogin} />}
+    </>
   );
 }
+
 
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [currentPage, setCurrentPage] = useState("blank");
   const [showSideNav, setShowSideNav] = useState(true);
 
-  const login = async (username: string, password: string) => {
+  const login = async (email: string, password: string) => {
     const response = await fetch(`${BACKEND_URL}login`, {
       method: 'POST',
-      body: String(JSON.stringify({ username, password })),
+      body: String(JSON.stringify({ email, password })),
       headers: {
         'Content-Type': 'application/json', 
       },
       credentials: 'include',
     });
   
-    if (response.ok) {
-      setIsAuthenticated(true);
-    } else {
-      alert('Login failed');
-    }
+      if (response.ok) {
+        setIsAuthenticated(true);
+      } else {
+        const error = await response.json().then(x=>x.error)
+        alert(error)
+      }
   };
 
   function showTrain(){
@@ -362,7 +481,7 @@ export default function Home() {
           <Content currentPage={currentPage} leftPos={showSideNav ? "left-60" : "left-0"}/>
         </>
       ) : (
-        <LoginPage onLogin={login} setIsAuthenticated={setIsAuthenticated}/>
+        <LoginOrSignupPage onLogin={login} setIsAuthenticated={setIsAuthenticated}/>
       )}
     </>
   );
